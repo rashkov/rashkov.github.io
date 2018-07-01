0431b560ee7281077c95724b41b81b964aee1ab8
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-07-01 04:22:49 -0400

    Update

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,1 +1,5 @@
+jekyll build
+git add .
+git commit -am "Update"
+git push
 git subtree push --prefix "_site" origin gh-pages
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 jekyll build
 git add .
-gco "Update"
+git commit -am "Update"
 git push
 git subtree push --prefix "_site" origin gh-pages
